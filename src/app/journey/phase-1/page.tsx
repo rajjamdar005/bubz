3aea510b0d1e7b85aa10eb10b2146acb7862684c
rajjamdar005 <143905609+rajjamdar005@users.noreply.github.com>
2024-08-03 14:46:38 +0530

    Update page.tsx

--- src/app/journey/phase-1/page.tsx
+++ src/app/journey/phase-1/page.tsx
@@ -10,9 +10,9 @@ const Page = () => {
     '/images/beg4.jpg'
   ];
 
-  const commonDescription =<p>"It all began in a workshop, where fate intervened as our instructor directed me to sit beside her. With a mix of excitement and nerves, I took my seat, and we quickly struck up a conversation. Before long, we exchanged Snapchat usernames, marking the start of our casual friendship.
+  const commonDescription =<p>It all began in a workshop, where fate intervened as our instructor directed me to sit beside her. With a mix of excitement and nerves, I took my seat, and we quickly struck up a conversation. Before long, we exchanged Snapchat usernames, marking the start of our casual friendship.
 As the workshop progressed, we were assigned a task: to create a bookmark as homework. I found myself struggling with the project, and to my delight, she offered to help. Our chats about the assignment soon turned into lively conversations, filled with laughter and shared ideas.
-When the second day of the workshop arrived, the air was electric with anticipation. We spent the day having fun, our connection deepening with every exchange. What began as a simple assignment had blossomed into something much more meaningful. This was just the beginning of our journey together, a journey that promised adventure and joy.";
+When the second day of the workshop arrived, the air was electric with anticipation. We spent the day having fun, our connection deepening with every exchange. What began as a simple assignment had blossomed into something much more meaningful. This was just the beginning of our journey together, a journey that promised adventure and joy.;
 </p>
   return (
     <div className={styles.container}>
